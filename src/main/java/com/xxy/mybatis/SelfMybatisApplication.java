e6b47209581e8ad5a6b5b7c55e57df388625341f
72116253 <154655197@qq.com>
2021-06-28 20:05:39 +0800

    mybatis初始化提交

---
+++ src/main/java/com/xxy/mybatis/SelfMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.xxy.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfMybatisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xxy/mybatis/SelfMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxy.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
